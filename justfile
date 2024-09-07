dfff3a67ec36293108aa19ed0937656406ea1442
Timon Vonk <timonv@gmail.com>
2024-09-07 20:58:46 +0200

    Add rust bench and benchmark script

--- justfile
+++ justfile
@@ -3,3 +3,10 @@ download-rust-book:
   cd data/rust-book && \
   curl https://codeload.github.com/rust-lang/book/tar.gz/main | \
   tar -xz --strip=2 book-main/src
+
+benchmark-rust-book: download-rust-book
+  hyperfine -w 1 \
+  -n langchain "cd langchain-bench && poetry run cli -d rust-book -q lanchain-rust-book" \
+  -n swiftide "cd swiftide-bench && cargo run --release -- -d rust-book -c swiftide-rust-book"
+
+  
--- swiftide-bench/src/main.rs
+++ swiftide-bench/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use swiftide::{
+    indexing::{
+        loaders::FileLoader,
+        transformers::{ChunkMarkdown, Embed},
+        EmbeddedField, Pipeline,
+    },
+    integrations::{fastembed::FastEmbed, qdrant::Qdrant},
+};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    dataset: String,
+    #[arg(short, long)]
+    collection_name: String,
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let args = Args::parse();
+
+    let qdrant = Qdrant::builder()
+        .collection_name(&args.collection_name)
+        .vector_size(384)
+        .with_vector(EmbeddedField::Combined)
+        .build()
+        .unwrap();
+
+    let client = qdrant.client();
+    let _ = client.delete_collection(args.collection_name);
+
+    Pipeline::from_loader(
+        FileLoader::new(format!("../data/{}", args.dataset)).with_extensions(&["md"]),
+    )
+    // .then_chunk(ChunkMarkdown::from_chunk_range(10..2024))
+    .then_in_batch(256, Embed::new(FastEmbed::try_default().unwrap()))
+    .then_store_with(qdrant)
+    .run()
+    .await
+    .unwrap();
 }
